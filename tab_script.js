13d073fe7a58302fb1386cf5b1dea39751064c4d
Andrey Starostin <itmyaddress@gmail.com>
2012-06-09 23:36:37 +0300

    initial add tab_grouper

---
+++ tab_script.js
@@ -0,0 +1,112 @@
+(function() {
+
+var tab = new Tab();
+window.addEventListener(
+	"load",
+	function()
+	{
+		tab.onLoad.call(tab);
+	},
+	false
+);
+
+function Tab()
+{
+	var _self = this;
+	this._groupButton = null;
+
+	this.onLoad = function()
+	{
+		this._groupButton = opera.contexts.toolbar.createItem({
+			title: "Tab Grouper",
+			icon: "icons/tab_group_button.png",
+			badge: {
+				backgroundColor: '#CC0000',
+				color: '#FFFFFF'
+			},
+			onclick: function() {
+				_self.onGroupClick.call(_self);
+			}
+		});
+
+		opera.contexts.toolbar.addItem(_self._groupButton);
+	};
+
+	this.onGroupClick = function()
+	{
+		var i = 0;
+		var similar = _self.getSimilar();
+		var tabGroups = opera.extension.tabGroups.getAll();
+		for (i = 0; i < tabGroups.length; i++)
+		{
+			var tabGroup = tabGroups[i];
+			var groupTabs = tabGroup.tabs.getAll();
+			if (groupTabs.length > 0)
+			{
+				similar[this._getSign(groupTabs[0].url)].group = tabGroup;
+			}
+		}
+
+		for (var sign in similar)
+		{
+			if (!similar.hasOwnProperty(sign))
+				continue;
+
+			var element = similar[sign];
+			if (element.list.length > 1)
+			{
+				if (!element.group)
+				{
+					opera.extension.tabGroups.create(element.list, {collapsed: true}, element.list[0]);
+				} else {
+					for (i = 0; i < element.list.length; i++)
+					{
+						tab = element.list[i];
+						if (!tab.tabGroup)
+							element.group.insert(tab);
+					}
+				}
+			}
+		}
+	};
+
+	this.getSimilar = function()
+	{
+		var similar = {};
+		var tabs = opera.extension.tabs.getAll();
+		for (var i = 0; i < tabs.length; i++)
+		{
+			var tab = tabs[i];
+			if (!tab.browserWindow)
+			{
+				continue;
+			}
+
+			var sign = this._getSign(tab.url);
+
+			if (!similar[sign])
+			{
+				similar[sign] = {
+					group: null,
+					list: []
+				};
+			}
+			similar[sign].list.push(tab);
+		}
+		return similar;
+	};
+
+	this._getSign = function(url)
+	{
+		var sign = "";
+		var match = [];
+		if ((match = url.match(/https?:\/\/([^\/]*)\//)) != null)
+		{
+			sign = match[1];
+		}
+
+		return sign;
+	};
+}
+
+})();
